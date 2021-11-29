b355cd715d5e66088a63c0e7ea88ccd9e35de5c6
antspr <antoinesprings@outlook.com>
2021-11-29 12:40:37 -0500

    added corsheaders settings

--- youtube_clone_comments/youtube_clone_comments/settings.py
+++ youtube_clone_comments/youtube_clone_comments/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'Comments',
     'Replies',
     
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'youtube_clone_comments.urls'
@@ -130,3 +132,4 @@ try:
 
 except ImportError:
     pass
+CORS_ORIGIN_ALLOW_ALL = True
